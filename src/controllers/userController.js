671bcb7085efa153a0b482539c491c36c2bd235a
paulinacabrera8 <nanacabrera13@gmail.com>
2022-01-11 16:59:43 -0300

    contraseñas encriptadas :)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ const req = require('express/lib/request');
 const path = require('path');
 const jsonDB = require ('../model/jsonDatabase');
 const productModel = jsonDB('user');
+const bcrypt= require('bcryptjs')
 
 
 const userController = {
@@ -9,43 +10,25 @@ const userController = {
     register:(req,res) => {
         res.render('register')
     },
-    // registerPost: (req, res) => {
-    //     let user = {
-    //         userNombre: req.body.registerNombre,
-    //         userApellido: req.body.registerApellido,
-    //         userEmail: req.body.registerEmail,
-    //         userPassword:req.body.registerPassword,
-    //         userPasswordRepeat:req.body.registerPasswordRepeat,
-    //         userRol:req.body.registerRol
-    //     }
-    //     let errores = validationResult(req)
-    //     if(errores.isEmpty()){
-    //     const registerUser = modelController.create(user)
-    //     res.redirect("/")
-    // }else{
-    //     res.render("users/register", {
-    //         errores:errores.mapped(),
-    //         old: req.body
-    //     })
-    // } 
-
-    login:(req,res) => {
-        res.render('login');
-    },
     create: (req,res)=>{
         let usuario = {
             nombre: req.body.nombre,
             apellido: req.body.apellido,
             email: req.body.email,
-            contraseña: req.body.contraseña,
+            contraseña: bcrypt.hashSync(req.body.contraseña),
             fecha: req.body.fecha
         }
         productModel.create(usuario);
         res.redirect('/')
-    }
+    },
+    
+
+    login:(req,res) => {
+        res.render('login');
+    },
+    
 }
 
-// }
 
 
 module.exports = userController;
