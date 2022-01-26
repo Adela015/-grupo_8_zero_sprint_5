70f398138605b35d2215bdde60fcfa802049194f
paulinacabrera8 <nanacabrera13@gmail.com>
2022-01-26 18:47:18 -0300

    Validaciones

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,6 +22,17 @@ const userController = {
         productModel.create(usuario);
         res.redirect('/')
     },
+    processRegister:(req, res) =>{
+        const result = validationResult(req)
+        if (result.errors.length > 0) {
+            return res.render('register', {
+                errors: result.mapped(),
+                oldData: req.body
+            });
+            
+        }
+        
+    },
     
 
     login:(req,res) => {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,18 +3,30 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 const { body } = require('express-validator')
 
+
 const validationsLogin = [
     body('correo').notEmpty().isEmail().withMessage('Debes ingresar un correo electrónico válido'),
     body('contra').notEmpty().withMessage('Debes ingresar una contraseña')
 ]
 
+const validations =[
+    body('nombre').notEmpty().withMessage('Debes ingresar tu nombre'),
+    body('apellido').notEmpty().withMessage('Debes ingresar tu apellido'),
+    body('email').notEmpty().isEmail().withMessage('Debes ingresar un correo electrónico válido'),
+    body('contraseña').notEmpty().withMessage('Debes ingresar una contraseña válida').isLength({min:8}).withMessage('La contraseña debe ser mas larga')
+] 
+
+//Formulario de registro
 
 router.get('/register', userController.register) //http://localhost:3000/user/register
+
+router.post('/register', validations , userController.processRegister)
 
 router.post('/register', userController.create)
 
 router.get('/login', userController.login)//http://localhost:3000/user/login
 
+//procesa el regristro
 router.post('/login', validationsLogin, userController.access)
 
 
